258d363eb87951275de658b1a029291b4727e08d
Philip Voldsgaard <philipvoldsgaard@gmail.com>
2018-01-09 09:46:11 +0100

    api_conn.py updated to retrieve data in proper format

--- api_conn.py
+++ api_conn.py
@@ -1,15 +1,18 @@
 from APIs.Poloniexlib import Poloniex
 import pickle
+import pandas as pd
+
 
 class APIConn(object):
 	def __init__(self, exchange, live=False, read_only=True):
 		
 		self.live = live
 		self.read_only = read_only
+		self.exchange = exchange
 		self.trades = {}
 		self.num_trades = 0
 		
-		if exchange == "poloniex":
+		if self.exchange == "poloniex":
 			with open(r"poloniex.pickle", "rb") as key_file:
 				keys = pickle.load(key_file)
 			if self.read_only:
@@ -20,10 +23,10 @@ class APIConn(object):
 				_secret = keys["trader_secret"]
 			self.conn = Poloniex(_public, _secret)
 
-	def get_last_price(self, pair):
+	def get_price_data(self, pair):
 		current_values = self.conn.api_query("returnTicker")
-		last_price = current_values[pair]["last"]  # This is specific for Poloniex -- Needs fix
-		return last_price
+		last_prices = current_values[pair]  # ["last"]  # This is specific for Poloniex -- Needs fix
+		return last_prices
 	
 	def execute_trade(self, trade):  # BUY amount of BASE/last at rate
 		if self.read_only:
@@ -40,8 +43,16 @@ class APIConn(object):
 	
 	def get_data(self, pair, start, end, frequency):
 		""" Returns a list of prices """
-		data = self.conn.api_query("returnChartData", {"currencyPair": pair, "start": start, "end": end, "period": frequency})
-		return data
+		if self.exchange == "poloniex":
+			data = self.conn.api_query("returnChartData", {"currencyPair": pair, "start": start, "end": end, "period": frequency})['candleStick']
+			df = pd.DataFrame(data)
+			df.index = df['date']
+			df = df.drop(["close", "date", "high", "low", "open", "quoteVolume", "volume"], axis=1)
+			df.columns = ["Price"]
+			df = df.sort_index()
+		else:
+			raise NotImplementedError("WARNING: THE EXCHANGE USED DOES NOT HAVE THIS FUNCTIONALITY YET!")
+		return df
 	
 	def return_balance(self, ccy):
 		out = self.conn.api_query("returnBalances")  # Poloniex specific -- Needs fix
